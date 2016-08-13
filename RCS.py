0ede2efa8eddd7efbaf9830d91a0e0de2205058d
Anmol Mago <anmolmago@gmail.com>
2016-08-13 11:42:08 -0400

    Add blacklist + improve accuracy

--- RCS.py
+++ RCS.py
@@ -13,29 +13,34 @@ def drawLine(img, rho, theta, color):
     y2 = int(y0 - 1000*(a))
     cv2.line(img,(x1,y1),(x2,y2),color,2)
 
-cap = cv2.VideoCapture(0)
-thres = 100
 def main():
-    global thres
+    cap = cv2.VideoCapture(0)
+    thres = 100
+    isBlacklist = True
+    blacklist = {}
+    cannyBlacklist
+    count = 0
+    eth = 20
     while True:
         ret, frame = cap.read()
         img = cv2.resize(frame, None,fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
-        blur = cv2.GaussianBlur(img,(7,7),0)
+        blur = cv2.GaussianBlur(img,(15,15),0)
         gray = cv2.cvtColor(blur,cv2.COLOR_BGR2GRAY)
-        edges = cv2.Canny(gray,35,111,apertureSize = 3)
+        edges = cv2.Canny(gray,eth, eth*3,apertureSize = 3)
         lines = cv2.HoughLines(edges,1,np.pi/45,thres)
         
         if lines is not None:
 
             linesByAngle = {}
-            
-            if len(lines) < 30:
-                thres -= 2
-            if len(lines) > 80:
-                thres += 2
 
             for x in range(0, len(lines)):
                 for rho, theta in lines[x]:
+                    if (rho,theta) in blacklist:
+                        continue
+                        
+                    if isBlacklist:
+                        blacklist[(rho,theta)] = blacklist.get((rho,theta), 0) + 1
+
                     drawLine(img, rho, theta, (0,0,255))
                     angle = round(math.degrees(theta), -1)
                     if not angle in linesByAngle:
@@ -50,7 +55,7 @@ def main():
                 for i in range(0, len(values)):
                     found = False
                     for j in range(0, i):
-                        if abs(values[i][0] - values[j][0]) < 50:
+                        if abs(values[i][0] - values[j][0]) < 25 and len(lineGroups[values[j][2]]) <= 10:
                             lineGroups[values[j][2]].append(values[i])
                             values[i] = (values[i][0], values[i][1], values[j][2])
                             found = True
@@ -59,10 +64,15 @@ def main():
                         values[i] = (values[i][0], values[i][1], len(lineGroups))
                         lineGroups[len(lineGroups)] = [(values[i][0], values[i][1], len(lineGroups))]
 
+            if len(lineGroups) < 25:
+                thres -= 2
+            if len(lineGroups) > 30:
+                thres += 2
+
+            # print str(thres) + "|" + str(len(lineGroups)) + "|" + str(len(lines))
+
             for index, lines in lineGroups.iteritems():
                 # print green on avg of those rhos and thetas
-                if len(lines) < 3:
-                    continue
                 rho = 0
                 theta = 0
                 for l in lines:
@@ -79,6 +89,11 @@ def main():
 
         cv2.imshow('edges',edges)
         cv2.imshow('frame',img)
+        count += 1
+        if count >= 100 and isBlacklist:
+            isBlacklist = False
+            eth = 30
+            print "Stopped recording blacklist!!"
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
